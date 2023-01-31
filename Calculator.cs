730c68e4edb6049cd4db3d0deeea7143a6e771ac
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-31 10:54:32 +0530

    first commit

---
+++ Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleCalc
+{
+    public class Calculator
+    {
+        public int Calculate(int number1, int number2, string operation)
+        {
+            string nonNullOperation =
+                operation ?? throw new ArgumentNullException(nameof(operation));
+
+            
+
+            if (nonNullOperation == "/")
+            {
+                try
+                {
+                    return Divide(number1, number2);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("...logging...");
+                   
+
+            }
+            
+        }
+
+        private int Divide(int number, int divisor) => number / divisor;
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleCalc
+{
+    class ConsoleCalc
+    {
+        static void Main(string[] args)
+        {
+            AppDomain currentAppDomain = AppDomain.CurrentDomain;
+            currentAppDomain.UnhandledException
+                += new UnhandledExceptionEventHandler(HandleException);
+
+            WriteLine("Enter first number");
+            int number1 = int.Parse(ReadLine());
+
+            WriteLine("Enter second number");
+            int number2 = int.Parse(ReadLine());
+
+            WriteLine("Enter operation");
+            string operation = ReadLine().ToUpper();
+
+
+            var calculator = new Calculator();
+
+            try
+            {
+                int result = calculator.Calculate(number1, number2, operation);
+                DisplayResult(result);
+            }
+            catch (ArgumentNullException ex) when (ex.ParamName == "operation")
+            {
+                WriteLine($"Operation was not provided. {ex}");
+            }
+            catch (ArgumentNullException ex)
+            {
+                WriteLine($"An argument was null. {ex}");
+            }
+           
+            catch (Exception ex)
+            {
+                WriteLine($"Sorry, something went wrong. {ex}");
+            }
+            finally
+            {
+                WriteLine("...finally...");
+            }
+
+
+
+            WriteLine("\nPress enter to exit.");
+            ReadLine();
+        }
+
+        private static void DisplayResult(int result) => WriteLine($"Result is: {result}");
+
+        private static void HandleException(object sender, UnhandledExceptionEventArgs e)
+        {
+            WriteLine($"Sorry there was a problem and we need to close. Details: {e.ExceptionObject}");
+        }
+    }
+}
+
